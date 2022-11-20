1455fcac5a548252c04711a20e0fc2b9c5beb293
Simon Goller <neosam+github@posteo.de>
2022-11-20 09:04:07 +0100

    Use rapier to make the player walk

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,45 @@
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
-#[derive(Component)]
+#[derive(Resource)]
+pub struct GameAssets {
+    pub player: Handle<Scene>,
+    pub tree: Handle<Scene>,
+}
+
+#[derive(Component, Reflect)]
 pub struct Player;
 
-#[derive(Component)]
+#[derive(Bundle)]
+pub struct PlayerBundle {
+    scene_bundle: SceneBundle,
+    rigid_body: RigidBody,
+    collider: Collider,
+    player: Player,
+    velocity: Velocity,
+    character_controller: KinematicCharacterController,
+    locked_axes: LockedAxes,
+}
+impl PlayerBundle {
+    pub fn new(assets: &GameAssets) -> Self {
+        PlayerBundle {
+            scene_bundle: SceneBundle {
+                scene: assets.player.clone(),
+                ..Default::default()
+            },
+            rigid_body: RigidBody::Dynamic,
+            collider: Collider::capsule_y(1.0, 0.5),
+            player: Player,
+            velocity: Velocity::default(),
+            character_controller: KinematicCharacterController::default(),
+            locked_axes: LockedAxes::ROTATION_LOCKED,
+        }
+    }
+}
+
+#[derive(Component, Reflect)]
 pub struct CameraController {
     pub rotation_y: f32,
     pub rotation_x: f32,
@@ -41,6 +75,8 @@ fn main() {
             },
             ..Default::default()
         }))
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup)
         .add_system(camera_movement)
         .add_system(keyboard_input)
@@ -50,22 +86,24 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let player = commands
-        .spawn((
-            SceneBundle {
-                scene: asset_server.load("human.glb#Scene0"),
-                transform: Transform::from_xyz(0.0, 0.0, 0.0),
-                ..Default::default()
-            },
-            Player,
-        ))
-        .id();
+    let assets = GameAssets {
+        player: asset_server.load("human.glb#Scene0"),
+        tree: asset_server.load("tree.glb#Scene0"),
+    };
+    let player = commands.spawn(PlayerBundle::new(&assets)).id();
     commands.spawn((
         Camera3dBundle {
             transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         },
         CameraController::new(player),
+    ));
+
+    commands.spawn((
+        RigidBody::Fixed,
+        Collider::cuboid(1000.0, 0.1, 1000.0),
+        Transform::from_xyz(0.0, -2.0, 0.0),
+        GlobalTransform::default(),
     ));
 
     commands.spawn(SceneBundle {
@@ -118,35 +156,37 @@ pub fn apply_camera_position(
 pub fn keyboard_input(
     keys: Res<Input<KeyCode>>,
     camera_query: Query<&CameraController>,
-    mut entity_position_query: Query<&mut Transform, Without<CameraController>>,
+    mut entity_position_query: Query<(&mut Transform, &mut Velocity), Without<CameraController>>,
 ) {
     if keys.pressed(KeyCode::W) {
         if let Ok(camera_controller) = camera_query.get_single() {
-            if let Ok(mut look_at_transform) =
+            if let Ok((mut look_at_transform, mut velocity)) =
                 entity_position_query.get_mut(camera_controller.lock_entity)
             {
-                let direction = look_at_transform.translation
-                    + Vec3::new(
-                        camera_controller.rotation_y.sin(),
-                        0.0,
-                        camera_controller.rotation_y.cos(),
-                    );
+                let vector = Vec3::new(
+                    camera_controller.rotation_y.sin(),
+                    0.0,
+                    camera_controller.rotation_y.cos(),
+                );
+                let direction = look_at_transform.translation + vector;
                 look_at_transform.look_at(direction, Vec3::Y);
+                velocity.linvel = -vector;
             }
         }
     }
     if keys.pressed(KeyCode::S) {
         if let Ok(camera_controller) = camera_query.get_single() {
-            if let Ok(mut look_at_transform) =
+            if let Ok((mut look_at_transform, mut velocity)) =
                 entity_position_query.get_mut(camera_controller.lock_entity)
             {
-                let direction = look_at_transform.translation
-                    - Vec3::new(
-                        camera_controller.rotation_y.sin(),
-                        0.0,
-                        camera_controller.rotation_y.cos(),
-                    );
+                let vector = Vec3::new(
+                    camera_controller.rotation_y.sin(),
+                    0.0,
+                    camera_controller.rotation_y.cos(),
+                );
+                let direction = look_at_transform.translation - vector;
                 look_at_transform.look_at(direction, Vec3::Y);
+                velocity.linvel = vector;
             }
         }
     }
